7cc25d8d0287e14f96fcbf94228fc6d7cfa5855f
digsribeiro <digsribeiro@outlook.com>
2020-11-16 12:06:49 -0300

    Initial Commit

---
+++ EventosMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventosMobile
+//
+//  Created by Diego Ribeiro on 16/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
